d51dfae0f9f17d2dcd13645f04a8d187d3a696ea
Erick Garcia <Erick.Garcia@coxinc.com>
2015-03-06 15:50:43 -0600

    agrego todo el proyecto otra vez corriendo con django 1.6 y con el settings ya separado

---
+++ SistemaDiscusiones/SistemaDiscusiones/settings/base.py
@@ -0,0 +1,43 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '+h!zhqlbozxb_jn43^)4nc4u8*lhu@6@_2&jue3r(ug&aaog1p'
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'SistemaDiscusiones.urls'
+
+WSGI_APPLICATION = 'SistemaDiscusiones.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
